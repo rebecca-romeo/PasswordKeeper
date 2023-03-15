eeb0150d8c60a5b21fbf30de64cc289ca872e458
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-03-15 09:11:31 -0400

    seed and schema data update

---
+++ db/seeds/00_organizations.sql
@@ -0,0 +1,1 @@
+INSERT INTO organizations (name) VALUES ('Lighthouse Labs');
---
+++ db/seeds/03_accounts.sql
@@ -0,0 +1,12 @@
+INSERT INTO accounts (username, password, website, category_id) VALUES
+('LHLfacebook', 1234, 'www.facebook.com', 1),
+('LHLlinkedin', 1234, 'www.linkedin.com', 1),
+('LHLbamboo', 1234, 'www.bamboo.com', 2),
+('LHLharvest', 1234, 'www.harvest.com', 2),
+('LHLsnapchat', 1234, 'www.snapchat.com', 3),
+('LHLreddit', 1234, 'www.reddit.com', 3),
+('LHLudemy', 1234, 'www.udemy.com', 4),
+('LHLcoursea', 1234, 'www.coursea.com', 4);
+
+
+
