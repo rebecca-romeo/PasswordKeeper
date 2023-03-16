1f4f5f51f647fb16e1d396fdcbc6ad680758d869
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-03-16 17:11:35 -0400

    display all accounts to page, copy button, and basic homepage with login button

--- db/seeds/03_accounts.sql
+++ db/seeds/03_accounts.sql
@@ -1,12 +1,12 @@
 INSERT INTO accounts (username, password, website, category_id) VALUES
-('LHLfacebook', 1234, 'www.facebook.com', 1),
-('LHLlinkedin', 1234, 'www.linkedin.com', 1),
-('LHLbamboo', 1234, 'www.bamboo.com', 2),
-('LHLharvest', 1234, 'www.harvest.com', 2),
-('LHLsnapchat', 1234, 'www.snapchat.com', 3),
-('LHLreddit', 1234, 'www.reddit.com', 3),
-('LHLudemy', 1234, 'www.udemy.com', 4),
-('LHLcoursea', 1234, 'www.coursea.com', 4);
+('LHLfacebook', 'fh9f', 'www.facebook.com', 1),
+('LHLlinkedin', 'ucn39', 'www.linkedin.com', 1),
+('LHLbamboo', '89f44', 'www.bamboo.com', 2),
+('LHLharvest', '6ufe', 'www.harvest.com', 2),
+('LHLsnapchat', 'f5ed', 'www.snapchat.com', 3),
+('LHLreddit', '8hvbe', 'www.reddit.com', 3),
+('LHLudemy', '8vbh', 'www.udemy.com', 4),
+('LHLcoursea', 'huvr3', 'www.coursea.com', 4);
 
 
 
